e17d29a6464574a9b217db2f9fd955b2e4c2797c
Sergio León <sergio.leon@analyticalways.com>
2019-12-23 11:42:18 +0100

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,16 @@ module.exports = (env, argv) => {
                         }
                     }
                 },
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }                
             ]
         },
         plugins: [
