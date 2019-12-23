a1baef1c41dac854e3e0c4dd065cb71d8128e21e
panicoenlaxbox <panicoenlaxbox@gmail.com>
2019-12-23 11:42:18 +0100

    babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 require('./styles/styles.scss');
 
 console.log('Hello World!');
+
+document.getElementById("babel").addEventListener("click", () => {
+    console.log(['Sergio', 'panicoenlaxbox'].map(n => n.toUpperCase()));
+});
