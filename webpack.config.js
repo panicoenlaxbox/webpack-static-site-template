6a9c3618a6ee6a151110ed982250a6edfaa91390
Sergio León <sergio.leon@analyticalways.com>
2019-12-23 11:07:47 +0100

    include partials

--- webpack.config.js
+++ webpack.config.js
@@ -4,68 +4,76 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    }
-                ],
-            },
-            {
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
 
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {     
-                            esModule: false,                       
-                            limit: 8192
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         }
-                    },
-                    'image-webpack-loader'
-                ]
-            },
-            {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader'
-                }
-            },            
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new webpack.SourceMapDevToolPlugin()
-    ],
-    mode: 'development'
+                    ],
+                },
+                {
+
+                    test: /\.(png|jpe?g|gif|svg)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                esModule: false,
+                                limit: 8192
+                            }
+                        },
+                        'image-webpack-loader'
+                    ]
+                },
+                {
+                    test: /\.(html)$/,
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: isProduction,
+                            interpolate: true,
+                        }
+                    }
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: 'src/index.html'
+            }),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin(),
+            new webpack.SourceMapDevToolPlugin()
+        ],
+        mode: 'development'
+    }
 };
